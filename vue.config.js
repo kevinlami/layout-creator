512fa585ae0e75ff4133ab487f42833b89f90dcc
Kevin Lami <kevinlami15@gmail.com>
2023-01-29 18:54:07 -0300

    feat: build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/layout-creator/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
